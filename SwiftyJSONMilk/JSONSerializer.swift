262d3cd4de5e4b385bbf7e50dcf0877d2e2799fa
Jordan Hamill <jordan@shopkeep.com>
2015-09-06 22:09:06 +0100

    Git ignore

---
+++ SwiftyJSONMilk/JSONSerializer.swift
@@ -0,0 +1,13 @@
+//
+//  JSONSerializer.swift
+//  SwiftyJSONMilk
+//
+//  Created by Jordan Hamill on 06/09/2015.
+//  Copyright © 2015 JordanHamill. All rights reserved.
+//
+
+import UIKit
+
+class JSONSerializer: NSObject {
+
+}
